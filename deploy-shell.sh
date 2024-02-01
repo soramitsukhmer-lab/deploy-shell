af54b41b9d5d2d96e2431ca439fbd2aadf34b620
Socheat Sok <alex@socheat.net>
2024-02-01 16:13:24 +0700

    Delete deploy-shell.sh

--- deploy-shell.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-_playbook=$(basename `pwd`)
-docker run -it --rm \
-		-v "ansible-${_playbook}:/root/.ansible" \
-		-v "$(pwd):$(pwd)" \
-		--workdir="$(pwd)" \
-	ghcr.io/soramitsukhmer-lab/deploy-shell:main
