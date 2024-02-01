93dceec924a07beb385172aa4ce5b7c222fd6fab
Socheat Sok <alex@socheat.net>
2024-02-01 16:24:20 +0700

    Add ability to detect and mount .ssh directory

--- docs/deploy-shell.sh
+++ docs/deploy-shell.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 set -e
-_playbook=$(basename `pwd`)
+workdir=$(pwd)
+playbook=$(basename "${workdir}")
+_docker_run_args=()
+
+# Mount ".ssh" directory locate in the host HOME directory.
+# If the playbook directory contains a ".ssh" directory, it will take precedence
+if [ -d "${workdir}.ssh" ]; then
+	_docker_run_args+=(-v "${workdir}.ssh:/root/.ssh:ro")
+elif [ -d "$HOME/.ssh" ]; then
+	_docker_run_args+=(-v "$HOME/.ssh:/root/.ssh:ro")
+fi
+
+echo "Starting deploy-shell container..."
+echo " - Working directory: ${workdir}"
+echo " - Playbook: ${playbook}"
+echo ""
+set -x
 docker run -it --rm \
-		-v "ansible-${_playbook}:/root/.ansible" \
-		-v "$(pwd):$(pwd)" \
-		--workdir="$(pwd)" \
+		-v "ansible-${playbook}:/root/.ansible" \
+		-v "${workdir}:${workdir}" \
+		--workdir="${workdir}" \
+		"${_docker_run_args[@]}" \
 	ghcr.io/soramitsukhmer-lab/deploy-shell:main
