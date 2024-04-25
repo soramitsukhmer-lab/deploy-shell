5f1b559f6af785f26af1bc708a6c653abf745ef7
Socheat Sok <alex@socheat.net>
2024-04-25 17:30:29 +0700

    Update docs/dev.sh

--- docs/dev.sh
+++ docs/dev.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
-workdir=$(pwd)
-playbook=$(basename "${workdir}")
 
-_supported_versions=("dev" "main" "v8" "v9")
-_deploy_shell_version="${1:-dev}"
-_deploy_shell_image="${2:-soramitsukhmer-lab/deploy-shell}"
-if [[ ! " ${_supported_versions[@]} " =~ " ${_deploy_shell_version} " ]]; then
-	echo "Unsupported deploy-shell version: ${_deploy_shell_version}"
-	echo "Supported versions: ${_supported_versions[*]}"
+DEPLOYSHELL_WORKDIR=$(pwd)
+DEPLOYSHELL_PLAYBOOK=$(basename "${DEPLOYSHELL_WORKDIR}")
+
+__SUPPORTED_VERSIONS=("dev", "main" "v8" "v9")
+__DEPLOYSHELL_VERSION="${1:-main}"
+__DEPLOYSHELL_IMAGE="${2:-ghcr.io/soramitsukhmer-lab/deploy-shell}"
+if [[ ! " ${__SUPPORTED_VERSIONS[@]} " =~ " ${__DEPLOYSHELL_VERSION} " ]]; then
+	echo "Unsupported deploy-shell version: ${__DEPLOYSHELL_VERSION}"
+	echo "Supported versions: ${__SUPPORTED_VERSIONS[*]}"
 	exit 1
 fi
 
-_docker_run_args=(
-	-v "ansible-${playbook}:/root/.ansible"
-	-v "${workdir}:/overlayfs/${workdir}"
-	--workdir="/overlayfs/${workdir}"
+_DOCKER_CID_FILE="$HOME/.deploy-shell.cid"
+
+_DOCKER_RUN_ARGS=(
+	-v "ansible-${DEPLOYSHELL_PLAYBOOK}:/root/.ansible"
+	-v "${DEPLOYSHELL_WORKDIR}:/overlayfs/${DEPLOYSHELL_WORKDIR}"
+	--cidfile "$_DOCKER_CID_FILE"
+	--workdir="/overlayfs/${DEPLOYSHELL_WORKDIR}"
 )
 
 # Mount ".ssh" directory locate in the host HOME directory.
 # If the playbook directory contains a ".ssh" directory, it will take precedence
-if [ -d "${workdir}.ssh" ]; then
-	_docker_run_args+=(-v "${workdir}.ssh:/root/.ssh:ro")
+if [ -d "${DEPLOYSHELL_WORKDIR}.ssh" ]; then
+	_DOCKER_RUN_ARGS+=(-v "${DEPLOYSHELL_WORKDIR}.ssh:/root/.ssh:ro")
 elif [ -d "$HOME/.ssh" ]; then
-	_docker_run_args+=(-v "$HOME/.ssh:/root/.ssh:ro")
+	_DOCKER_RUN_ARGS+=(-v "$HOME/.ssh:/root/.ssh:ro")
 fi
 
 echo "Welcome to @soramitsukhmer-lab/deploy-shell!"
@@ -30,18 +34,22 @@ echo ""
 echo "A virtual deployment shell for DevOps for @soramitsukhmer"
 echo "Simplify the process of using Ansible and other tools for DevOps"
 echo ""
-echo "  Version: ${_deploy_shell_version}"
-echo "  Working directory: ${workdir}"
+echo "  Version: ${__DEPLOYSHELL_VERSION}"
+echo "  Working directory: ${DEPLOYSHELL_WORKDIR}"
 echo ""
 
-echo "Checking for updates..."
-docker pull \
-	${_deploy_shell_image}:${_deploy_shell_version}
-
 echo ""
-echo "Starting deploy-shell container..."
-docker run -it --rm "${_docker_run_args[@]}" \
-	${_deploy_shell_image}:${_deploy_shell_version}
+if [ -f "$_DOCKER_CID_FILE" ]; then
+	echo "Reusing existing session..."
+	docker exec -it $(cat "$_DOCKER_CID_FILE") zsh
+	rm "$_DOCKER_CID_FILE"
+else
+	echo "Checking for updates..."
+	docker pull ${__DEPLOYSHELL_IMAGE}:${__DEPLOYSHELL_VERSION}
+
+	echo "Starting deploy-shell container..."
+	docker run -it --rm "${_DOCKER_RUN_ARGS[@]}" ${__DEPLOYSHELL_IMAGE}:${__DEPLOYSHELL_VERSION}
+fi
 
 echo ""
 echo "..............................."
