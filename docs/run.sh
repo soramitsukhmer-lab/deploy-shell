0d7a27d3fa7cd117812a353ec3e28aa164ed8f59
Socheat Sok <alex@socheat.net>
2024-04-25 17:20:39 +0700

    Add ability to reuse already exists session

--- docs/run.sh
+++ docs/run.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
-workdir=$(pwd)
-playbook=$(basename "${workdir}")
 
-_supported_versions=("main" "v8" "v9")
-_deploy_shell_version="${1:-main}"
-_deploy_shell_image="${2:-ghcr.io/soramitsukhmer-lab/deploy-shell}"
-if [[ ! " ${_supported_versions[@]} " =~ " ${_deploy_shell_version} " ]]; then
-	echo "Unsupported deploy-shell version: ${_deploy_shell_version}"
-	echo "Supported versions: ${_supported_versions[*]}"
+DEPLOYSHELL_WORKDIR=$(pwd)
+DEPLOYSHELL_PLAYBOOK=$(basename "${DEPLOYSHELL_WORKDIR}")
+
+_SUPPORTED_VERSIONS=("main" "v8" "v9")
+_DEPLOYSHELL_VERSION="${1:-main}"
+_DEPLOYSHELL_IMAGE="${2:-ghcr.io/soramitsukhmer-lab/deploy-shell}"
+if [[ ! " ${_SUPPORTED_VERSIONS[@]} " =~ " ${_DEPLOYSHELL_VERSION} " ]]; then
+	echo "Unsupported deploy-shell version: ${_DEPLOYSHELL_VERSION}"
+	echo "Supported versions: ${_SUPPORTED_VERSIONS[*]}"
 	exit 1
 fi
 
-_docker_run_args=(
-	-v "ansible-${playbook}:/root/.ansible"
-	-v "${workdir}:/overlayfs/${workdir}"
-	--workdir="/overlayfs/${workdir}"
+_DOCKER_CID_FILE="$HOME/.deploy-shell.cid"
+
+_DOCKER_RUN_ARGS=(
+	--cidfile "$_DOCKER_CID_FILE"
+	-v "ansible-${DEPLOYSHELL_PLAYBOOK}:/root/.ansible"
+	-v "${DEPLOYSHELL_WORKDIR}:/overlayfs/${DEPLOYSHELL_WORKDIR}"
+	--workdir="/overlayfs/${DEPLOYSHELL_WORKDIR}"
 )
 
 # Mount ".ssh" directory locate in the host HOME directory.
 # If the playbook directory contains a ".ssh" directory, it will take precedence
-if [ -d "${workdir}.ssh" ]; then
-	_docker_run_args+=(-v "${workdir}.ssh:/root/.ssh:ro")
+if [ -d "${DEPLOYSHELL_WORKDIR}.ssh" ]; then
+	_DOCKER_RUN_ARGS+=(-v "${DEPLOYSHELL_WORKDIR}.ssh:/root/.ssh:ro")
 elif [ -d "$HOME/.ssh" ]; then
-	_docker_run_args+=(-v "$HOME/.ssh:/root/.ssh:ro")
+	_DOCKER_RUN_ARGS+=(-v "$HOME/.ssh:/root/.ssh:ro")
 fi
 
 echo "Welcome to @soramitsukhmer-lab/deploy-shell!"
@@ -30,18 +34,20 @@ echo ""
 echo "A virtual deployment shell for DevOps for @soramitsukhmer"
 echo "Simplify the process of using Ansible and other tools for DevOps"
 echo ""
-echo "  Version: ${_deploy_shell_version}"
-echo "  Working directory: ${workdir}"
+echo "  Version: ${_DEPLOYSHELL_VERSION}"
+echo "  Working directory: ${DEPLOYSHELL_WORKDIR}"
 echo ""
 
 echo "Checking for updates..."
-docker pull \
-	${_deploy_shell_image}:${_deploy_shell_version}
+docker pull ${_DEPLOYSHELL_IMAGE}:${_DEPLOYSHELL_VERSION}
 
 echo ""
 echo "Starting deploy-shell container..."
-docker run -it --rm "${_docker_run_args[@]}" \
-	${_deploy_shell_image}:${_deploy_shell_version}
+if [ -f "$_DOCKER_CID_FILE" ]; then
+	docker exec -it $(cat "$_DOCKER_CID_FILE") zsh
+else
+	docker run -it --rm "${_DOCKER_RUN_ARGS[@]}" ${_DEPLOYSHELL_IMAGE}:${_DEPLOYSHELL_VERSION}
+fi
 
 echo ""
 echo "..............................."
