ef4111fd510950103f62d00ae1ad3e850eb67604
Socheat Sok <4363857+socheatsok78@users.noreply.github.com>
2024-05-04 17:13:47 +0700

    Refactor docs/run.sh script

--- docs/run.sh
+++ docs/run.sh
@@ -1,60 +1,162 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-DEPLOYSHELL_WORKDIR=$(pwd)
-DEPLOYSHELL_PLAYBOOK=$(basename "${DEPLOYSHELL_WORKDIR}")
+DEPLOYSHELL_CONTAINER_TAG="ghcr.io/soramitsukhmer-lab/deploy-shell:main"
 
-__SUPPORTED_VERSIONS=("main" "v8" "v9")
-__DEPLOYSHELL_VERSION="${1:-main}"
-__DEPLOYSHELL_IMAGE="${2:-ghcr.io/soramitsukhmer-lab/deploy-shell}"
-if [[ ! " ${__SUPPORTED_VERSIONS[@]} " =~ " ${__DEPLOYSHELL_VERSION} " ]]; then
-	echo "Unsupported deploy-shell version: ${__DEPLOYSHELL_VERSION}"
-	echo "Supported versions: ${__SUPPORTED_VERSIONS[*]}"
-	exit 1
-fi
+DEPLOYSHELL_ROOT="$HOME/.deploy-shell"
+DEPLOYSHELL_CID="$DEPLOYSHELL_ROOT/current"
+DEPLOYSHELL_SSH_AUTH_SOCK="$DEPLOYSHELL_ROOT/ssh-auth.sock"
+DEPLOYSHELL_PWD=$(pwd)
+DEPLOYSHELL_WORKDIR=$(basename "${DEPLOYSHELL_PWD}")
+DEPLOYSHELL_SUPPORTED_VERSIONS=("main" "v8" "v9")
 
-_DOCKER_CID_FILE="$HOME/.deploy-shell.cid"
-
-_DOCKER_RUN_ARGS=(
-	-v "ansible-${DEPLOYSHELL_PLAYBOOK}:/root/.ansible"
-	-v "${DEPLOYSHELL_WORKDIR}:/overlayfs/${DEPLOYSHELL_WORKDIR}"
-	--cidfile "$_DOCKER_CID_FILE"
-	--workdir="/overlayfs/${DEPLOYSHELL_WORKDIR}"
-)
-
-# Mount ".ssh" directory locate in the host HOME directory.
-# If the playbook directory contains a ".ssh" directory, it will take precedence
-if [ -d "${DEPLOYSHELL_WORKDIR}.ssh" ]; then
-	_DOCKER_RUN_ARGS+=(-v "${DEPLOYSHELL_WORKDIR}.ssh:/root/.ssh:ro")
-elif [ -d "$HOME/.ssh" ]; then
-	_DOCKER_RUN_ARGS+=(-v "$HOME/.ssh:/root/.ssh:ro")
-fi
-
-echo "Welcome to @soramitsukhmer-lab/deploy-shell!"
-echo ""
-echo "A virtual deployment shell for DevOps for @soramitsukhmer"
-echo "Simplify the process of using Ansible and other tools for DevOps"
-echo ""
-echo "  Version: ${__DEPLOYSHELL_VERSION}"
-echo "  Working directory: ${DEPLOYSHELL_WORKDIR}"
-echo ""
-
-echo ""
-if [ -f "$_DOCKER_CID_FILE" ]; then
-	echo "Reusing existing session..."
-	docker exec -it $(cat "$_DOCKER_CID_FILE") zsh
+# helpers
+# string formatters
+if [[ -t 1 ]]; then
+	tty_escape() { printf "\033[%sm" "$1"; }
 else
-	echo "Checking for updates..."
-	docker pull ${__DEPLOYSHELL_IMAGE}:${__DEPLOYSHELL_VERSION}
-
-	echo "Starting deploy-shell container..."
-	docker run -it --rm "${_DOCKER_RUN_ARGS[@]}" ${__DEPLOYSHELL_IMAGE}:${__DEPLOYSHELL_VERSION}
+	tty_escape() { :; }
 fi
 
-# Clean up
-rm "$_DOCKER_CID_FILE"
+tty_mkbold() { tty_escape "1;$1"; }
+tty_underline="$(tty_escape "4;39")"
+tty_blue="$(tty_mkbold 34)"
+tty_red="$(tty_mkbold 31)"
+tty_yellow="$(tty_mkbold 33)"
+tty_bold="$(tty_mkbold 39)"
+tty_reset="$(tty_escape 0)"
 
-echo ""
-echo "..............................."
-echo "You have exited the virtual deployment shell!"
-echo "Have a nice day!"
-exit 0
+shell_join() {
+	local arg
+	printf "%s" "$1"
+	shift
+	for arg in "$@"; do
+		printf " "
+		printf "%s" "${arg// /\ }"
+	done
+}
+
+chomp() {
+	printf "%s" "${1/"$'\n'"/}"
+}
+
+ohai() {
+	printf "${tty_blue}==>${tty_bold} %s${tty_reset}\n" "$(shell_join "$@")"
+}
+
+warn() {
+	printf "${tty_yellow}Warning${tty_reset}: %s\n" "$(chomp "$1")"
+}
+
+error(){
+	printf "${tty_red}Error${tty_reset}: %s\n" "$(chomp "$1")" >>/dev/stderr
+}
+
+abort() {
+  printf "%s\n" "$1"
+  exit 1
+}
+
+execute() {
+  if ! "$@"; then
+	abort "$(printf "Failed during: %s" "$(shell_join "$@")")"
+  fi
+}
+
+function print_welcome() {
+	ohai "Welcome to @soramitsukhmer-lab/deploy-shell!"
+	ohai ""
+	ohai "A virtual deployment shell for DevOps for @soramitsukhmer"
+	ohai "Simplify the process of using Ansible and other tools for DevOps"
+}
+
+function print_options() {
+	ohai ""
+}
+
+function main() {
+	local INPUT_VERSION=${1:-"main"}
+	local INPUT_CONTAINER_TAG=$2
+
+	# Check input version
+	if [[ -z "${INPUT_CONTAINER_TAG}" ]]; then
+		if [[ ! " ${DEPLOYSHELL_SUPPORTED_VERSIONS[@]} " =~ " ${INPUT_VERSION} " ]]; then
+			error "Unsupported version: ${INPUT_VERSION}, supported versions: ${DEPLOYSHELL_SUPPORTED_VERSIONS[*]}"
+			exit 2
+		fi
+	else
+		DEPLOYSHELL_CONTAINER_TAG="${INPUT_CONTAINER_TAG}:${INPUT_VERSION}"
+	fi
+
+	# Print welcome message
+	print_welcome
+	echo ""
+
+	# Do pre-cleanup if needed
+	if [[ -n "${DEPLOYSHELL_FORCE_CLEANUP}" ]]; then
+		deployshell_stop
+		deployshell_cleanup
+	fi
+
+	# Check if the deploy-shell root directory exists
+	execute mkdir -p "${DEPLOYSHELL_ROOT}"
+
+	# Check if the deploy-shell container ID file exists
+	# If it exists, connect to the existing session
+	# Otherwise, check for updates and start a new session
+	if [ -f "${DEPLOYSHELL_CID}" ]; then
+		ohai "Connecting to existing session..."
+		execute docker exec -it $(cat "${DEPLOYSHELL_CID}") zsh
+	else
+		local DOCKER_RUN_ARGS=(
+			--cidfile "${DEPLOYSHELL_CID}"
+			--workdir "/overlayfs/${DEPLOYSHELL_PWD}"
+			-v "${DEPLOYSHELL_PWD}:/overlayfs/${DEPLOYSHELL_WORKDIR}"
+			-v "deploy-shell-ansible-${DEPLOYSHELL_WORKDIR}:/root/.ansible"
+		)
+
+		ohai "Prepare container environment..."
+		# TBD
+
+		if [[ -z "$DEPLOYSHELL_SKIP_UPDATE" ]]; then
+			ohai "Check for updates..."
+			execute docker pull "${DEPLOYSHELL_CONTAINER_TAG}"
+		fi
+
+		ohai "Starting deploy-shell container..."
+		echo "$ docker run -it --rm  ${DOCKER_RUN_ARGS[@]}"
+		echo ""
+		docker run -it --rm "${DOCKER_RUN_ARGS[@]}" "${DEPLOYSHELL_CONTAINER_TAG}"
+		if [[ $? -ne 0 ]]; then
+			error "Failed to start deploy-shell container!"
+			exit 3
+		fi
+	fi
+}
+
+function deployshell_stop() {
+	if [ -f "$DEPLOYSHELL_CID" ]; then
+		(cat "${DEPLOYSHELL_CID}" | xargs docker kill -s SIGTERM) >/dev/null
+		(cat "${DEPLOYSHELL_CID}" | xargs docker rm -f) >/dev/null
+	fi
+	
+}
+function deployshell_cleanup {
+	if [ -d "$DEPLOYSHELL_ROOT" ]; then
+		rm -rf "${DEPLOYSHELL_ROOT}" >/dev/null || true
+	fi
+}
+
+function tran_EXIT() {
+	deployshell_cleanup
+	exit 0
+}
+
+function tran_SIGHUP() {
+	deployshell_stop
+	deployshell_cleanup
+	exit 0
+}
+
+trap tran_EXIT EXIT
+trap tran_SIGHUP HUP
+main "$@"
